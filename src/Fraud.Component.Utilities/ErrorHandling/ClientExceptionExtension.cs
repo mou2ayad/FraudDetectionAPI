7b0268af2b8b6050a108ac917c78ddb73e597980
Mouayad Khashfeh <mouayad.khashfeh@knab.nl>
2021-12-20 05:19:34 +0100

    wip

--- src/Fraud.Component.Utilities/ErrorHandling/ClientExceptionExtension.cs
+++ src/Fraud.Component.Utilities/ErrorHandling/ClientExceptionExtension.cs
@@ -10,29 +10,10 @@ namespace Fraud.Component.Utilities.ErrorHandling
                 exception.Data.Add("ClientException", true);
             return exception;
         }
-        public static Exception MarkAsNotClientException(this Exception exception)
-        {
-            if (exception.Data.Contains("ClientException"))
-                exception.Data.Remove("ClientException");
-            return exception;
-        }
-        public static bool IsClientException(this Exception exception) => exception.Data.Contains("ClientException");
-        
-        public static Exception WithNoLog(this Exception exception)
-        {
-            if (!exception.Data.Contains("WithNoLog"))
-                exception.Data.Add("WithNoLog", true);
-            return exception;
-        }
-        public static Exception WithLog(this Exception exception)
-        {
-            if (exception.Data.Contains("WithNoLog"))
-                exception.Data.Remove("WithNoLog");
 
-            return exception;
-        }
+        public static bool IsClientException(this Exception exception) => exception.Data.Contains("ClientException");
+
         public static bool IsWithNoLog(this Exception exception) => exception.Data.Contains("WithNoLog");
-              
         
         public static Exception LogAsInfo(this Exception exception)
         {
